e0beed667d166021961c3c2f5bdaec39efefafe6
Nisar <nisarahmed8901@gmail.com>
2023-11-29 11:00:45 -0700

    add hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+def f():
+    return 1
+
+value = 100
+
+name = 1
+print('this will run')
+
+# if__name__ == "__main__":
+#     print('hello world!')
+#     a = 1
+#     if a > 0:
+#         print('hi')
